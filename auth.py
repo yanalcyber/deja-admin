a2dda263009e7e37957b18f12e6be0ad780f8a38
yanalcyber <yanaldeep05@gmail.com>
2026-03-06 19:51:36 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,8 +2,8 @@ import streamlit as st
 from database import supabase
 
 def show_login_screen():
-    st.title("🔐 مرحباً بك في نظام Deja")
-    st.markdown("يرجى تسجيل الدخول أو إنشاء حساب للوصول إلى لوحة التحكم.")
+    st.title("مرحبا بكم في فريق deja الالكتروني ")
+    st.markdown("يرجى تسجيل الدخول أو إنشاء حساب للدخول .")
     
     tab1, tab2 = st.tabs(["تسجيل الدخول", "إنشاء حساب جديد"])
     
@@ -47,3 +47,4 @@ def show_login_screen():
                     st.error(f"❌ حدث خطأ، تأكد من كلمة المرور (6 أحرف) وأن الرقم غير مسجل. (الخطأ: {str(e)})")
             else:
                 st.warning("يرجى تعبئة جميع الحقول.")
+
